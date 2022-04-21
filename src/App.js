a035493607bf4d8cbad8dabf0334b6e9e740dc07
Matias Valdez <matiasvaldez8184@gmail.com>
2022-04-21 13:13:43 -0300

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes,Navigate } from "react-router-dom";
 import Home from "./components/Home/Home";
 import EmpresaDetail from "./components/EmpresaDetail/EmpresaDetail";
 
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
       <Routes>
+        <Route exact path="/" element={<Navigate to="/empresas"/>} />
         <Route exact path="/empresas" element={<Home />} />
         <Route path="/empresas/:id" element={<EmpresaDetail />}/>
       </Routes>
